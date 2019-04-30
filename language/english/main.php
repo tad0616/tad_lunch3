433f68fdf177cc64a77acd8c6eded1d69eb214b2
Tad <tad0616@gmail.com>
2019-04-30 12:42:05 +0800

    加語系

--- language/english/main.php
+++ language/english/main.php
@@ -1,21 +1,5 @@
 <?php
-/**
- * Tad Lunch3 module
- *
- * You may not change or alter any portion of this comment or credits
- * of supporting developers from this source code or any supporting source code
- * which is considered copyrighted (c) material of the original comment or credit authors.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * @copyright  The XOOPS Project http://sourceforge.net/projects/xoops/
- * @license    http://www.fsf.org/copyleft/gpl.html GNU public license
- * @package    Tad Lunch3
- * @since      2.5
- * @author     tad
- * @version    $Id $
- **/
+xoops_loadLanguage('main', 'tadtools');
 define('_TAD_NEED_TADTOOLS', "Need modules/tadtools. You can download tadtools from <a href='http://www.tad0616.net/modules/tad_uploader/index.php?of_cat_sn=50' target='_blank'>Tad's web</a>.");
 define('_MD_TADLUNCH3_SMNAME1', 'Home');
 
